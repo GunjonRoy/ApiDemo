62ece4c27d8e92d2c58fb57793ba59fb2fd6218c
GunjonRoy <01gunjonroy5722@gmail.com>
2021-09-07 17:13:02 +0600

    1st

--- lib/ProductDataModel.dart
+++ lib/ProductDataModel.dart
@@ -1,28 +1,40 @@
-class ProductDataModel{
+class ProductDataModel {
   int? id;
-  String? name;
-  String? category;
-  String? imageURL;
-  String? oldPrice;
-  String? price;
+  String? productName;
+  String? productBrand;
+  String? productImg;
+  String? quantity;
+  String? createdAt;
+  String? updatedAt;
 
   ProductDataModel(
-      {
-        this.id,
-      this.name,
-      this.category,
-      this.imageURL,
-      this.oldPrice,
-      this.price
-      });
+      { this.id,
+        this.productName,
+        this.productBrand,
+        this.productImg,
+        this.quantity,
+        this.createdAt,
+        this.updatedAt});
 
-  ProductDataModel.fromJson(Map<String,dynamic> json)
-  {
+  ProductDataModel.fromJson(Map<String, dynamic> json) {
     id = json['id'];
-    name =json['name'];
-    category = json['category'];
-    imageURL = json['imageUrl'];
-    oldPrice = json['oldPrice'];
-    price = json['price'];
+    productName = json['product_name'];
+    productBrand = json['product_brand'];
+    productImg = json['product_img'];
+    quantity = json['quantity'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['product_name'] = this.productName;
+    data['product_brand'] = this.productBrand;
+    data['product_img'] = this.productImg;
+    data['quantity'] = this.quantity;
+    data['created_at'] = this.createdAt;
+    data['updated_at'] = this.updatedAt;
+    return data;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'package:http/http.dart' as http;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:read_json_file/ProductDataModel.dart';
@@ -14,9 +14,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
@@ -25,8 +25,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -35,64 +33,103 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(
-        future: ReadJsonData(),
-        builder: (context,data){
-          if(data.hasError){
-            return Center(child: Text("${data.error}"));
-          }else if(data.hasData){
-            var items =data.data as List<ProductDataModel>;
-            return ListView.builder(
-              itemCount: items == null? 0: items.length,
-                itemBuilder: (context,index){
-                  return Card(
-                    elevation: 5,
-                    margin: EdgeInsets.symmetric(horizontal: 10,vertical: 6),
-                    child: Container(
-                      padding: EdgeInsets.all(8),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            child: Image(image: NetworkImage(items[index].imageURL.toString()),fit: BoxFit.fill,),
+        body: FutureBuilder(
+      future: ReadJsonData(),
+      builder: (context, data) {
+        if (data.hasError) {
+          return Center(child: Text("${data.error}"));
+        } else if (data.hasData) {
+          var items = data.data as List<ProductDataModel>;
+          return ListView.builder(
+              itemCount: items == null ? 0 : items.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          child: Image(
+                            image:
+                                NetworkImage(items[index].productImg.toString()),
+                            fit: BoxFit.fill,
                           ),
-                          Expanded(child: Container(
-                            padding: EdgeInsets.only(bottom: 8),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Padding(padding: EdgeInsets.only(left: 8,right: 8),child: Text(items[index].name.toString(),style: TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold
-                                ),),),
-                                Padding(padding: EdgeInsets.only(left: 8,right: 8),child: Text(items[index].price.toString()),)
-                              ],
-                            ),
-                          ))
-                        ],
-                      ),
+                        ),
+                        Expanded(
+                            child: Container(
+                          padding: EdgeInsets.only(bottom: 8),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(left: 8, right: 8),
+                                child: Text(
+                                  items[index].productName.toString(),
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 8, right: 8),
+                                child: Text(items[index].productBrand.toString()),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 8, right: 8),
+                                child: Text(items[index].quantity.toString()),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 8, right: 8),
+                                child: Text(items[index].createdAt.toString()),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 8, right: 8),
+                                child: Text(items[index].updatedAt.toString()),
+                              )
+                            ],
+                          ),
+                        ))
+                      ],
                     ),
-                  );
-                }
-            );
-          }else{
-            return Center(child: CircularProgressIndicator(),);
-          }
-        },
-      )
-    );
+                  ),
+                );
+              });
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    ));
   }
 
-  Future<List<ProductDataModel>>ReadJsonData() async{
-     final jsondata = await rootBundle.rootBundle.loadString('jsonfile/productlist.json');
-     final list = json.decode(jsondata) as List<dynamic>;
+  Future<List<ProductDataModel>> ReadJsonData() async {
+    final response = await http.get("https://api.sakkhar.com/api/product");
+    if (response.statusCode == 200) {
+      List responseList = json.decode(response.body);
+      return responseList
+          .map((data) => ProductDataModel.fromJson(data))
+          .toList();
+    } else {
+      // If that call was not successful, throw an error.
+      throw Exception('Failed to load post');
+    }
 
-     return list.map((e) => ProductDataModel.fromJson(e)).toList();
+    //http.Response response=await http.get("https://api.sakkhar.com/api/product");
+//    final jsondata =
+//        await rootBundle.rootBundle.loadString('jsonfile/productlist.json');
+
+//    final list = json.decode(response.body) as List<dynamic>;
+//
+//    return list.map((e) {
+//      return ProductDataModel.fromJson(e);
+//    }).toList();
   }
-  }
-
-
+}
